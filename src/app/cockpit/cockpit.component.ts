d5ae6514a06ed75a0560b41400ae54cb880058fa
Ritesh Ghule <ritesh.ghule@scsoft.com>
2023-12-23 20:59:25 +0530

    Used Local reference in template instead by two way binding in cockpit.html

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -8,9 +8,9 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
 
   @Output() serverCreated = new EventEmitter<{serverName:string, serverContent:string}>();
-  @Output() blueprintCreated = new EventEmitter<{serverName:string, serverContent:string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName:string, serverContent:string}>();
 
-  newServerName = '';
+  // newServerName = '';
   newServerContent = '';
 
   constructor() { }
@@ -18,12 +18,14 @@ export class CockpitComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAddServer() {
-    this.serverCreated.emit({serverName:this.newServerName, serverContent:this.newServerContent})
+  onAddServer(nameInput:HTMLInputElement) {
+    console.log(nameInput.value);
+    
+    this.serverCreated.emit({serverName:nameInput.value, serverContent:this.newServerContent})
   }
 
-onAddBlueprint() {
-  this.blueprintCreated.emit({serverName:this.newServerName, serverContent:this.newServerContent})
+onAddBlueprint(nameInput:HTMLInputElement) {
+  this.blueprintCreated.emit({serverName:nameInput.value, serverContent:this.newServerContent})
 
 }
 
